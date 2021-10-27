a9ecaf306042acd8dd9a707166325e2917a25d76
Rupinder <rupinder.developer@gmail.com>
2021-10-27 19:24:28 +0530

    Remove third party safearea view package

--- components/canvas/index.js
+++ components/canvas/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, StatusBar } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 
 const BackdropView = styled.View`
@@ -8,15 +8,15 @@ const BackdropView = styled.View`
 `;
 
 const expandBoundsStyle = {
-  height: "100%",
-  width: "100%",
+  height: '100%',
+  width: '100%'
 };
 
 export default function Canvas({ children }) {
   return (
     <BackdropView style={expandBoundsStyle}>
+      <StatusBar barStyle="light-content" />
       <SafeAreaView style={expandBoundsStyle}>
-        <StatusBar barStyle="light-content" />
         {children}
       </SafeAreaView>
     </BackdropView>
