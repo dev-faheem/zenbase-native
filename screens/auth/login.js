c154fbfc695c8c03e94e4fa2ee7d045914d8b59d
Sampan Verma <sampan.developer@gmail.com>
2021-12-07 02:59:39 +0530

    Added Registration APIs

--- screens/auth/login.js
+++ screens/auth/login.js
@@ -1,40 +1,40 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Text, Container, Canvas, Button } from "components";
 import { StackActions, CommonActions } from "@react-navigation/native";
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 import { useAuth } from "stores/auth";
 import { useTheme } from "stores/theme";
 import { TouchableOpacity } from "react-native";
 
 // Import Images
-import ZentbaseLogoPrimary from 'assets/images/zenbase-full-primary-logo.png';
+import ZentbaseLogoPrimary from "assets/images/zenbase-full-primary-logo.png";
+import { useLoader } from "stores/loader";
 
 // Styled Component
 const ZenbaseLogo = styled.Image`
   width: 176px;
   height: 48px;
-  margin-top: ${props => props.theme.spacing.md};
-`
+  margin-top: ${(props) => props.theme.spacing.md};
+`;
 const InputWrapper = styled.View`
   width: 100%;
   margin-top: 40px;
-`
+`;
 
 const Input = styled.TextInput`
   width: 100%;
   height: 40px;
-  padding: ${props => props.theme.spacing.sm};
-  border-radius: ${props => props.theme.borderRadius.md};
-  background-color: ${props => props.theme.color.hud};
-  color: ${props => props.theme.color.white};
-  margin-top: ${props => props.theme.spacing.md};
-`
-
+  padding: ${(props) => props.theme.spacing.sm};
+  border-radius: ${(props) => props.theme.borderRadius.md};
+  background-color: ${(props) => props.theme.color.hud};
+  color: ${(props) => props.theme.color.white};
+  margin-top: ${(props) => props.theme.spacing.md};
+`;
 
 const FooterWrapper = styled.View`
   width: 100%;
   height: 190px;
-`
+`;
 
 const FooterFlex = styled.View`
   flex: 1;
@@ -42,7 +42,7 @@ const FooterFlex = styled.View`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-`
+`;
 
 const TermsAndPrivacyWrapper = styled.View`
   flex: 1;
@@ -50,16 +50,15 @@ const TermsAndPrivacyWrapper = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding-top: ${props => props.theme.spacing.lg};
-`
+  padding-top: ${(props) => props.theme.spacing.lg};
+`;
 
 const TermsAndPrivacyFlex = styled.View`
   flex-direction: row;
   justify-content: flex-start;
-`
+`;
 
 export default function Login({ navigation }) {
-  
   const { login } = useAuth();
   const { theme } = useTheme();
 
@@ -67,13 +66,14 @@ export default function Login({ navigation }) {
 
   // States
   const [isLoginEnabled, setIsLoginEnabled] = useState(false);
-  const [phoneNumberOrEmail, setPhoneNumberOrEmail] = useState('');
-  const [password, setPassword] = useState('')
+  const [phoneNumberOrEmail, setPhoneNumberOrEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setLoading } = useLoader();
 
   // Input Handler
   const updateInput = (setState, value) => {
     setState(value);
-  }
+  };
 
   // Login Handler
   const loginHandler = () => {
@@ -82,33 +82,31 @@ export default function Login({ navigation }) {
     // Reset Stack Navigation
     navigation.dispatch(
       CommonActions.reset({
-        routes: [
-          { name: 'App' },
-        ],
+        routes: [{ name: "App" }],
       })
     );
-  }
+  };
 
   useEffect(() => {
-    if (phoneNumberOrEmail.trim() == '' || password == '') {
+    if (phoneNumberOrEmail.trim() == "" || password == "") {
       setIsLoginEnabled(false);
     } else {
       setIsLoginEnabled(true);
     }
-  }, [phoneNumberOrEmail, password])
-
-
+  }, [phoneNumberOrEmail, password]);
 
   return (
     <Canvas>
       <Container style={{ flex: 1 }}>
-        <Text fontSize='34' fontWeight='bold' style={{ marginTop: 10 }}>Meditate, Earn, Repeat</Text>
+        <Text fontSize="34" fontWeight="bold" style={{ marginTop: 10 }}>
+          Meditate, Earn, Repeat
+        </Text>
         <ZenbaseLogo source={ZentbaseLogoPrimary} />
 
         <InputWrapper>
           <Input
-            autoCapitalize='none'
-            placeholder='Phone number or email'
+            autoCapitalize="none"
+            placeholder="Phone number or email"
             placeholderTextColor={theme.color.secondary}
             onChangeText={(value) => updateInput(setPhoneNumberOrEmail, value)}
             value={phoneNumberOrEmail}
@@ -116,7 +114,7 @@ export default function Login({ navigation }) {
           />
 
           <Input
-            placeholder='Password'
+            placeholder="Password"
             placeholderTextColor={theme.color.secondary}
             onChangeText={(value) => updateInput(setPassword, value)}
             secureTextEntry={true}
@@ -124,30 +122,57 @@ export default function Login({ navigation }) {
             ref={passwordInput}
           />
 
-          <Button onPress={() => navigation.navigate('ForgotPassword')} variant='silent' fontSize='14' title='Forgot Password?' style={{ marginTop: 8 }} />
+          <Button
+            onPress={() => navigation.navigate("ForgotPassword")}
+            variant="silent"
+            fontSize="14"
+            title="Forgot Password?"
+            style={{ marginTop: 8 }}
+          />
         </InputWrapper>
       </Container>
       <FooterWrapper>
         <Container style={{ flex: 1 }}>
           <FooterFlex>
-            <Button onPress={() => navigation.navigate('Register')} variant='silent' fontSize='14' title='Create an account' style={{ marginTop: 8, marginBottom: 2 }} />
-            <Button variant={isLoginEnabled ? 'primary' : 'disabled'} title='Sign in' block onPress={() => {
-              if (isLoginEnabled) {
-                loginHandler();
-              }
-            }} />
+            <Button
+              onPress={() => navigation.navigate("Register")}
+              variant="silent"
+              fontSize="14"
+              title="Create an account"
+              style={{ marginTop: 8, marginBottom: 2 }}
+            />
+            <Button
+              variant={isLoginEnabled ? "primary" : "disabled"}
+              title="Sign in"
+              block
+              onPress={() => {
+                if (isLoginEnabled) {
+                  loginHandler();
+                }
+              }}
+            />
             <TermsAndPrivacyWrapper>
               <TermsAndPrivacyFlex>
                 <Text>By signing in you accept our </Text>
                 <TouchableOpacity>
-                  <Text fontWeight='bold' style={{ textDecorationLine: "underline" }}>Terms of use</Text>
+                  <Text
+                    fontWeight="bold"
+                    style={{ textDecorationLine: "underline" }}
+                  >
+                    Terms of use
+                  </Text>
                 </TouchableOpacity>
               </TermsAndPrivacyFlex>
 
               <TermsAndPrivacyFlex style={{ marginTop: 2 }}>
                 <Text>and </Text>
                 <TouchableOpacity>
-                  <Text fontWeight='bold' style={{ textDecorationLine: "underline" }}>Privacy Policy</Text>
+                  <Text
+                    fontWeight="bold"
+                    style={{ textDecorationLine: "underline" }}
+                  >
+                    Privacy Policy
+                  </Text>
                 </TouchableOpacity>
               </TermsAndPrivacyFlex>
             </TermsAndPrivacyWrapper>
--- screens/signup-bonus/index.js
+++ screens/signup-bonus/index.js
@@ -1,24 +1,31 @@
 import React, { useEffect } from "react";
-import { Container, Canvas, Text, Button, ZentTokenBanner, Box } from 'components';
-import styled from 'styled-components/native';
+import {
+  Container,
+  Canvas,
+  Text,
+  Button,
+  ZentTokenBanner,
+  Box,
+} from "components";
+import styled from "styled-components/native";
 import { useTheme } from "stores/theme";
-import { TouchableOpacity, Dimensions } from 'react-native';
-import ConfettiCannon from 'react-native-confetti-cannon';
-import * as Haptics from 'expo-haptics';
+import { TouchableOpacity, Dimensions } from "react-native";
+import ConfettiCannon from "react-native-confetti-cannon";
+import * as Haptics from "expo-haptics";
 
 // Import Icons
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 // Import Images
-import ConfettiImage from 'assets/images/confetti.png';
-
+import ConfettiImage from "assets/images/confetti.png";
+import { useAuth } from "stores/auth";
 
 // Styled Component
 
 const BackgroundImage = styled.ImageBackground`
-    width: 100%;
-    height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 
 const InfoWrapper = styled.View`
   flex: 1;
@@ -26,62 +33,93 @@ const InfoWrapper = styled.View`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`
+`;
 
 const InfoBody = styled.View`
   flex: 1;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const InfoFooter = styled.View`
   width: 100%;
   flex-direction: column;
-  padding-bottom: ${props => props.theme.spacing.lg};
-`
+  padding-bottom: ${(props) => props.theme.spacing.lg};
+`;
 
 const Header = styled.View`
-    width: 100%;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    margin-left: ${props => props.theme.spacing.sm};
-`
+  width: 100%;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin-left: ${(props) => props.theme.spacing.sm};
+`;
 
 // SignupBonus (Default)
 export default function SignupBonus({ route, navigation }) {
-    const { theme } = useTheme();
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
+  const { theme } = useTheme();
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
 
-    useEffect(() => {
-        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
-    }, [])
+  const { user, giveToken } = useAuth();
 
-    return (
-        <Canvas>
-            <BackgroundImage source={ConfettiImage} resizeMode='cover'>
-                <ConfettiCannon count={100} fallSpeed={2000} origin={{ x: windowWidth/2, y: windowHeight - 100 }} fadeOut />
-                <Header>
-                    <TouchableOpacity onPress={() => { navigation.goBack(); }}>
-                        <Ionicons name="ios-chevron-back" size={30} color={theme.color.primary} />
-                    </TouchableOpacity>
-                </Header>
-                <Container style={{ flex: 1 }}>
-                    <ZentTokenBanner tokens={0.01} usd={0.00} />
-                    <InfoWrapper>
-                        <InfoBody>
-                            <Ionicons name='gift' size={34} style={{ marginBottom: 12 }} color='white' />
-                            <Text fontSize='h2' fontWeight='bold'>You’ve received 0.01 ZENT</Text>
-                            <Text fontSize='md' style={{ marginTop: 5 }}>Thanks for creating an account!</Text>
-                        </InfoBody>
-                        <InfoFooter>
-                            <Button title='Claim to ' block onPress={() => { navigation.navigate('ReferFriends')}} />
-                        </InfoFooter>
-                    </InfoWrapper>
-                </Container>
-            </BackgroundImage>
-        </Canvas>
-    );
+  useEffect(() => {
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+  }, []);
+
+  return (
+    <Canvas>
+      <BackgroundImage source={ConfettiImage} resizeMode="cover">
+        <ConfettiCannon
+          count={100}
+          fallSpeed={2000}
+          origin={{ x: windowWidth / 2, y: windowHeight - 100 }}
+          fadeOut
+        />
+        <Header>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Ionicons
+              name="ios-chevron-back"
+              size={30}
+              color={theme.color.primary}
+            />
+          </TouchableOpacity>
+        </Header>
+        <Container style={{ flex: 1 }}>
+          <ZentTokenBanner tokens={0.01} usd={0.0} />
+          <InfoWrapper>
+            <InfoBody>
+              <Ionicons
+                name="gift"
+                size={34}
+                style={{ marginBottom: 12 }}
+                color="white"
+              />
+              <Text fontSize="h2" fontWeight="bold">
+                You’ve received 0.01 ZENT
+              </Text>
+              <Text fontSize="md" style={{ marginTop: 5 }}>
+                Thanks for creating an account{user?.name && `, ${user?.name}`}!
+              </Text>
+            </InfoBody>
+            <InfoFooter>
+              <Button
+                title="Claim token"
+                block
+                onPress={() => {
+                  giveToken(1, "registeration bonus");
+                  navigation.navigate("ReferFriends");
+                }}
+              />
+            </InfoFooter>
+          </InfoWrapper>
+        </Container>
+      </BackgroundImage>
+    </Canvas>
+  );
 }
