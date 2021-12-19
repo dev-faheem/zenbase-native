40307fba5b690525ea63cb6a7b051e4ab523d6b4
Rupinder <rupinder.developer@gmail.com>
2021-12-19 20:30:31 +0530

    Added Horizontal Scroll Song List

--- screens/search/index.js
+++ screens/search/index.js
@@ -9,9 +9,19 @@ import {
 } from "components";
 import styled from "styled-components/native";
 import useSearch from "queries/useSearch";
-import { FlatList, ScrollView } from "react-native";
+import { FlatList, ScrollView, Dimensions, TouchableOpacity } from "react-native";
 import useCategories from "queries/useCategories";
 
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { useTheme } from "stores/theme";
+
+
+// Import Images
+import SongImg from 'assets/images/song.png';
+import ArtistImg from 'assets/images/artist.png';
+
+
+// Styled Components
 const SearchInput = styled.TextInput`
   background-color: rgba(27, 28, 30, 0.9);
   color: rgba(143, 144, 148, 1);
@@ -23,7 +33,61 @@ const SearchInput = styled.TextInput`
   margin-bottom: 10px;
 `;
 
+const SongListWrapper = styled.View`
+  width: ${Dimensions.get('window').width * 0.89}px;
+  padding-top: ${props => props.theme.spacing.md};
+  padding-bottom: ${props => props.theme.spacing.md};
+`
+
+const SongList = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+const SongImage = styled.Image`
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+  border-radius: 5px;
+`;
+
+const ArtistImage = styled.Image`
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+  border-radius: 50px;
+`;
+
+const SongContentWrapper = styled.View`
+  height: 48px;
+  width: 83%;
+  flex-direction: row;
+  justify-content: space-between;
+  border-top-color: rgba(172, 178, 155, 0.5);
+  border-top-width: 0px;
+  
+  border-bottom-color: rgba(172, 178, 155, 0.5);
+  border-bottom-width: 0.5px;
+`
+
+const SongContent = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: ${props => props.theme.spacing.sm};
+`
+
+const IconWrapper = styled.View`
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+`
+
 export default function Search() {
+
+  const { theme } = useTheme();
+
   const [search, setSearch] = useState("");
   const searchQuery = useSearch();
   const categoriesQuery = useCategories();
@@ -47,11 +111,84 @@ export default function Search() {
             onChangeText={(value) => setSearch(value)}
           />
 
+
+          <Text fontSize="sm" color="secondary" style={{ marginTop: 5, marginBottom: 5 }}>
+            RECENT
+          </Text>
+
+          <ScrollView
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            style={{ width: '100%' }}
+            horizontal={true}>
+            <SongListWrapper >
+
+              <SongList onPress={() => { }}>
+                <SongImage source={SongImg} />
+                <SongContentWrapper style={{ borderTopWidth: 0.5 }}>
+
+                  <SongContent>
+                    <Text>Prime Time</Text>
+                    <Text fontSize='sm' color="secondary">Song • Aware</Text>
+                  </SongContent>
+
+                  <IconWrapper>
+                    <TouchableOpacity onPress={() => {}}>
+                      <Feather name="more-horizontal" size={24} color={theme.color.white} />
+                    </TouchableOpacity>
+                  </IconWrapper>
+                </SongContentWrapper>
+              </SongList>
+
+              <SongList onPress={() => { }}>
+                <ArtistImage source={ArtistImg} />
+                <SongContentWrapper>
+                  <SongContent>
+                    <Text>Karunesh</Text>
+                    <Text fontSize='sm' color="secondary">Artist</Text>
+                  </SongContent>
+
+                  <IconWrapper>
+                    <TouchableOpacity onPress={() => {}}>
+                      <Ionicons name="ios-chevron-forward" size={24} color={theme.color.white} />
+                    </TouchableOpacity>
+                  </IconWrapper>
+                </SongContentWrapper>
+              </SongList>
+              
+
+            </SongListWrapper>
+
+            {/* Last Wrapper */}
+            <SongListWrapper >
+
+              <SongList>
+                <SongImage source={SongImg} />
+
+                {/* set width to 88% if it is last list wrapper */}
+                <SongContentWrapper style={{ borderTopWidth: 0.5, width: '85%'}}>
+                  <SongContent>
+                    <Text>Prime Time</Text>
+                    <Text fontSize='sm' color="secondary">Song • Aware</Text>
+                  </SongContent>
+
+                  <IconWrapper>
+                    <Feather name="more-horizontal" size={24} color={theme.color.white} />
+                  </IconWrapper>
+                </SongContentWrapper>
+              </SongList>
+              
+
+            </SongListWrapper>
+          </ScrollView>
+
           {searchQuery?.data?.results?.length > 0 && (
             <Text fontSize="xs" color="secondary">
               RECENT
             </Text>
           )}
+
+          {/* <SongListing song={item} index={index} />; */}
 
           <FlatList
             data={searchQuery?.data?.results}
