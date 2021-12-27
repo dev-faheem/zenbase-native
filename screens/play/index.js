9e690eea0502e90468d205ce9265833dfda72e14
sampan-litmus7 <sampan.developer@gmail.com>
2021-12-27 05:43:20 +0530

    Updated play page

--- screens/play/index.js
+++ screens/play/index.js
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { Text, Container, Canvas, NavigationPadding } from "components";
-import { useRoute } from "@react-navigation/core";
-import styled from "styled-components";
-import axios from "services/axios";
-import { useLoader } from "stores/loader";
-import { Dimensions } from "react-native";
-import { Shadow } from "react-native-shadow-2";
+import React, { useEffect, useRef, useState } from 'react';
+import { Text, Container, Canvas, NavigationPadding } from 'components';
+import { useRoute } from '@react-navigation/core';
+import styled from 'styled-components';
+import axios from 'services/axios';
+import { useLoader } from 'stores/loader';
+import { Dimensions } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
+import Slider from '@react-native-community/slider';
+import {
+  FontAwesome5,
+  Foundation,
+  Ionicons,
+  MaterialCommunityIcons,
+  Feather,
+} from '@expo/vector-icons';
+import { Audio } from 'expo-av';
 
 const ScreenContainer = styled.ScrollView`
   padding-left: 22px;
@@ -31,8 +40,8 @@ const SongArtworkContainer = styled.View`
   margin-bottom: 27px;
 `;
 const SongArtwork = styled.Image`
-  width: ${Math.floor(Dimensions.get("window").width) - 22 - 22}px;
-  height: ${Math.floor(Dimensions.get("window").width) - 22 - 22}px;
+  width: ${Math.floor(Dimensions.get('window').width) - 22 - 22}px;
+  height: ${Math.floor(Dimensions.get('window').width) - 22 - 22}px;
   border-radius: 10px;
 `;
 const SongTitle = styled.Text`
@@ -44,25 +53,27 @@ const SongArtist = styled.Text`
   font-size: 22px;
   color: rgba(247, 248, 250, 0.6);
   font-weight: normal;
-  margin-top: -5px;
+  margin-top: 0px;
 `;
 
-const SongSeekbarTimeContainer = styled.View``;
-const SongSeekbarTime = styled.Text``;
+const SongControls = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+const SongControlsButton = styled.TouchableOpacity``;
 
-const SongPlaybackButtonsContainer = styled.View``;
-
-const SongVolumeControlContainer = styled.View``;
-const SongVolumeControlContainerIcon = styled.View``;
-
-const SongControls = styled.View``;
-const SongControlsButton = styled.View``;
+let audio = new Audio.Sound();
 
 export default function Play() {
   const route = useRoute();
   const { _id } = route.params;
 
   const [song, setSong] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(1);
+  const [position, setPosition] = useState(0);
+  const [duration, setDuration] = useState(1);
   const { setLoading, renderLoader } = useLoader();
 
   useEffect(() => {
@@ -73,8 +84,12 @@ export default function Play() {
     try {
       setLoading(true);
       const { data } = await axios.get(`/songs/${id}`);
-      setSong(data.data);
-      setLoading(false);
+      await setSong(data.data);
+      await setLoading(false);
+      await audio.unloadAsync();
+      setPosition(0);
+      setDuration(1);
+      setIsPlaying(false);
     } catch (e) {
       axios.handleError(e);
     }
@@ -86,15 +101,15 @@ export default function Play() {
       {song && (
         <>
           <SongBackdrop
-            source={{ uri: song.artwork?.replace("https", "http") }}
+            source={{ uri: song.artwork?.replace('https', 'http') }}
             blurRadius={40}
           />
           <SongArtworkContainer>
             <Shadow distance={50} radius={10}>
               <SongArtwork
-                source={{ uri: song.artwork?.replace("https", "http") }}
+                source={{ uri: song.artwork?.replace('https', 'http') }}
                 style={{
-                  shadowColor: "black",
+                  shadowColor: 'black',
                   shadowOffset: { height: 2 },
                   shadowOpacity: 1,
                 }}
@@ -102,8 +117,92 @@ export default function Play() {
             </Shadow>
           </SongArtworkContainer>
           <ScreenContainer>
-            <SongTitle>{song.name}</SongTitle>
-            <SongArtist>{song.artist?.name}</SongArtist>
+            <SongTitle>{song.name || 'Song Name'}</SongTitle>
+            <SongArtist>{song.artist?.name || 'Artist Name'}</SongArtist>
+
+            {/* Playback Slider / Seekbar */}
+            <Slider
+              minimumValue={0}
+              value={position}
+              maximumValue={duration}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+            />
+
+            <SongControls>
+              <SongControlsButton>
+                <FontAwesome5 name="backward" size={24} color="white" />
+              </SongControlsButton>
+
+              {isPlaying ? (
+                <SongControlsButton
+                  onPress={async () => {
+                    return;
+                    setIsPlaying(false);
+                    await audio.stopAsync();
+                  }}
+                >
+                  <Foundation name="pause" size={48} color="white" />
+                </SongControlsButton>
+              ) : (
+                <SongControlsButton
+                  onPress={async () => {
+                    await audio.unloadAsync();
+                    await audio.loadAsync({ uri: song.source });
+                    await audio.playAsync({ uri: song.source });
+
+                    audio.setOnPlaybackStatusUpdate((status) => {
+                      setDuration(status.durationMillis);
+                      setPosition(status.positionMillis);
+                    });
+
+                    setIsPlaying(true);
+                  }}
+                >
+                  <Foundation name="play" size={48} color="white" />
+                </SongControlsButton>
+              )}
+
+              <SongControlsButton>
+                <FontAwesome5 name="forward" size={24} color="white" />
+              </SongControlsButton>
+            </SongControls>
+
+            {/* Volume Slider */}
+            <Slider
+              thumbSize={{
+                width: 8,
+                height: 8,
+              }}
+              minimumValue={0}
+              maximumValue={1}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+              value={volume}
+              onValueChange={async (_volume) => {
+                await audio.setVolumeAsync(_volume);
+                setVolume(_volume);
+              }}
+            />
+
+            <SongControls>
+              <SongControlsButton>
+                <Ionicons name="ios-close-circle" size={24} color="white" />
+              </SongControlsButton>
+              <SongControlsButton>
+                <Ionicons name="heart-circle-sharp" size={24} color="white" />
+              </SongControlsButton>
+              <SongControlsButton>
+                <MaterialCommunityIcons
+                  name="meditation"
+                  size={24}
+                  color="white"
+                />
+              </SongControlsButton>
+              <SongControlsButton>
+                <Feather name="more-horizontal" size={24} color="white" />
+              </SongControlsButton>
+            </SongControls>
           </ScreenContainer>
         </>
       )}
