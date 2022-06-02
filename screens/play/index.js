ba4aa8da0f2e33068897742c37710f1225d7446e
Rupinder Singh <rupinder.developer@gmail.com>
2022-06-02 22:56:26 +0530

    Fix giveaway timer

--- screens/play/index.js
+++ screens/play/index.js
@@ -33,7 +33,7 @@ import { useAuth } from "stores/auth";
 import ReactNativeShare from "helpers/react-native-share";
 import { useSongQueue } from "stores/song-queue";
 
-const GIVEAWAY_TOKEN_AFTER_SECONDS = 5; // seconds
+const GIVEAWAY_TOKEN_AFTER_SECONDS = 5 * 60; // seconds
 // const GIVEAWAY_TOKEN_AFTER_SECONDS = 5; // seconds
 const CONTINUE_LISTENING = 60 * 60 * 1; //seconds
 
