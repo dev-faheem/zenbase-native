b1415668ba496e8b08ee48699c445f8db4ec67c9
mogulders <mogulders@gmail.com>
2022-06-06 14:43:15 -0700

    saving with working tracking transparency

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { queryClient } from "services/query";
 import { AuthProvider } from "stores/auth";
 import { ThemeProvider } from "stores/theme";
@@ -8,8 +8,19 @@ import { LoaderProvider } from "stores/loader";
 import { StripeProvider } from "@stripe/stripe-react-native";
 import config from "./config";
 import { SongQueueProvider } from "stores/song-queue";
+import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency';
 
 export default function App() {
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestTrackingPermissionsAsync();
+      if (status === 'granted') {
+        console.log('Yay! I have user permission to track data');
+      }
+    })();
+  }, []);
+
   return (
     <StripeProvider
       publishableKey={config.STRIPE_KEY}
